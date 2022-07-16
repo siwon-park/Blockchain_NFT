5529fc3b32e0b90f370de7fb1049ebb7e69599fa
siwon-park <zow777@naver.com>
2022-07-17 01:07:36 +0900

    220714 SW: Blockchain Basic learned

--- 0421_prac/musics/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Artist(models.Model):
-    name = models.CharField(max_length=200)
-
-class Music(models.Model):
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
