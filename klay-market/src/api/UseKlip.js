e9a3da6b268c8957d3c924908d74875f4a9d7571
siwon-park <zow777@naver.com>
2022-08-21 23:34:31 +0900

    05_Market BApp upload

--- klay-market/src/api/UseKlip.js
+++ klay-market/src/api/UseKlip.js
@@ -1,11 +1,15 @@
 import axios from "axios";
+import { COUNT_CONTRACT_ADDRESS } from '../constants';
 
-export const getAddress = (setQrValue) => {
+const A2P_API_PREPARE_URL = "https://a2a-api.klipwallet.com/v2/a2a/prepare";
+const APP_NAME = 'KLAY_MARKET';
+
+export const getAddress = (setQrValue, callback) => {
   
   axios.post(
-    "https://a2a-api.klipwallet.com/v2/a2a/prepare", {
+    A2P_API_PREPARE_URL, {
       bapp: {
-        name: 'KLAY_MARKET'
+        name: APP_NAME
       },
       type: 'auth'
     } 
@@ -18,7 +22,41 @@ export const getAddress = (setQrValue) => {
         if (res.data.result) {
           // console.log(`[RESULT] ${res.data.result}`);
           console.log(`[RESULT] ${JSON.stringify(res.data.result)}`);
+          callback(res.data.result.klaytn_address);
           clearInterval(timerId);
+        }
+      })
+    }, 1000)
+  })
+}
+
+export const setCount = (count, setQrValue) => {
+  
+  axios.post(
+    A2P_API_PREPARE_URL, {
+      bapp: {
+        name: APP_NAME,
+      },
+      type: "execute_contract",
+      transaction: {
+        to: COUNT_CONTRACT_ADDRESS,
+        value: "0",
+        abi: '{ "constant": false, "inputs": [ { "name": "_count", "type": "uint256" } ], "name": "setCount", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }', // 실행할 함수
+        params: `[\"${count}\"]`,
+      }
+    } 
+  ).then((response) => {
+    const { request_key } = response.data; // const request_key = response.data.request_key;
+    const qrcode = `https://klipwallet.com/?target=/a2a?request_key=${request_key}`
+    setQrValue(qrcode);
+    let timerId = setInterval(() => {
+      axios.get(`https://a2a-api.klipwallet.com/v2/a2a/result?request_key=${request_key}`).then((res) => {
+        if (res.data.result) {
+          // console.log(`[RESULT] ${res.data.result}`);
+          console.log(`[RESULT] ${JSON.stringify(res.data.result)}`);
+          if (res.data.result.status === 'success') {
+            clearInterval(timerId);
+          }
         }
       })
     }, 1000)
